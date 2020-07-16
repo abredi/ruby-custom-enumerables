319adbae182efff1f9cb0629e8647040cf98a442
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-07-17 02:59:58 +0300

    updated Enumarator #my_each linting issues

--- lib/main.rb
+++ lib/main.rb
@@ -1,27 +1,27 @@
 module Enumerable
   def my_each
     return enum_for unless block_given?
+
     array = is_a?(Range) ? to_a : self
 
-    for item in array
-      yield(item)
-    end
-    array
+    array.map { |item| yield(item) }
   end
 
   def my_each_with_index
     return enum_for unless block_given?
+
     array = is_a?(Range) ? to_a : self
 
     index = -1
     for item in array
-      yield(item, index+=1)
+      yield(item, index += 1)
     end
     array
   end
   
   def my_select
     return enum_for unless block_given?
+
     array = is_a?(Range) ? to_a : self
 
     filtered = []
@@ -40,6 +40,14 @@ module Enumerable
     flag
   end
 
+  def my_any?
+    flag = array.length > 0
+    array.my_each do
+      break if yield(item)
+    end
+    flag
+end
+
   def my_none?
     flag = true
     for item in array
@@ -52,12 +60,15 @@ module Enumerable
   def my_count(arg = nil)
     array = is_a?(Range) ? to_a : self
     if arg
-      return array.select{|val| val if val == arg}.length   
+      return array.select{ |val| val if val == arg }.length
     end
-    return array.length unless block_given?
+
     count = array.length
+
+    return count unless block_given?
+    
     for item in array
-      count-=1 unless yield(item)
+      count -= 1 unless yield(item)
     end
     count
   end
@@ -68,7 +79,7 @@ module Enumerable
     filtered = []
     if prok
       for item in array
-        filtered.push(prok.call(item)) 
+        filtered.push(prok.call(item))
       end
     else
       for item in array
@@ -78,22 +89,11 @@ module Enumerable
     
     filtered
   end
-
-  def my_each_with_index
-    return enum_for unless block_given?
-    array = is_a?(Range) ? to_a : self
-
-    index = -1
-    for item in array
-      yield(item, index+=1)
-    end
-    array
-  end
   
   def my_inject(init = nil)
     array = is_a?(Range) ? to_a : self
 
-    acc =  init
+    acc = init
 
     unless init.is_a?(Integer)
       acc = array[0]
@@ -114,9 +114,18 @@ module Enumerable
   end
 
   def multiply_els
-    self.my_inject { |mul, n| mul * n }
+    my_inject { |mul, n| mul * n }
   end
 
 end
 
-print [2,4,5].multiply_els
+hash = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5 }
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+# my_each
+hash.my_each do |value, key| 
+  print value, '-', key
+  puts ''
+end
+
+puts ''
